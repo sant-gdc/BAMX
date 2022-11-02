01d6a2afdcb4e5e96b1fb8ef60803f5437867256
AdrianBecerra411 <71723297+AdrianBecerra411@users.noreply.github.com>
2022-11-01 20:26:19 -0600

    Programs Screen
    Filled Programs Screen with dummy data, front end first iteration complete

---
+++ bamx_app/lib/dummy_data.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: unnecessary_const, constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+import './models/programs.dart';
+
+const DUMMY_CATEGORIES = const [
+  Program(
+    id: 'p1',
+    title: 'Colecta',
+    image:
+        'https://www.centronia.org/2016/wp-content/uploads/2016/11/DSCF7782-580x345.jpg?e7ce06&e7ce06',
+  ),
+  Program(
+    id: 'p2',
+    title: 'Colecta',
+    image:
+        'https://www.meganoticias.mx/uploads/noticias/fundacion-hara-colecta-de-alimentos-no-perecederos-para-familias-vulnerables-192023.jpeg',
+  ),
+  Program(
+    id: 'p3',
+    title: 'Acopio',
+    image: 'https://www.mees.com.mx/wp-content/uploads/2020/04/SPC-016.jpg',
+  ),
+  Program(
+    id: 'p4',
+    title: 'Colecta',
+    image:
+        'https://www.centronia.org/2016/wp-content/uploads/2016/11/DSCF7782-580x345.jpg?e7ce06&e7ce06',
+  ),
+  Program(
+    id: 'p5',
+    title: 'Colecta',
+    image:
+        'https://www.meganoticias.mx/uploads/noticias/fundacion-hara-colecta-de-alimentos-no-perecederos-para-familias-vulnerables-192023.jpeg',
+  ),
+  Program(
+    id: 'p6',
+    title: 'Acopio',
+    image: 'https://www.mees.com.mx/wp-content/uploads/2020/04/SPC-016.jpg',
+  ),
+  Program(
+    id: 'p7',
+    title: 'Colecta',
+    image:
+        'https://www.centronia.org/2016/wp-content/uploads/2016/11/DSCF7782-580x345.jpg?e7ce06&e7ce06',
+  ),
+  Program(
+    id: 'p8',
+    title: 'Colecta',
+    image:
+        'https://www.meganoticias.mx/uploads/noticias/fundacion-hara-colecta-de-alimentos-no-perecederos-para-familias-vulnerables-192023.jpeg',
+  ),
+  Program(
+    id: 'p9',
+    title: 'Acopio',
+    image: 'https://www.mees.com.mx/wp-content/uploads/2020/04/SPC-016.jpg',
+  ),
+];
---
+++ bamx_app/lib/programs_item.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class ProgramsItem extends StatelessWidget {
+  final String title;
+  final String image;
+
+  ProgramsItem(this.title, this.image);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(1, 3),
+          )
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        //   image: DecorationImage(
+        //     image: NetworkImage(image),
+        //   ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * .12,
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadiusDirectional.only(
+                  topStart: Radius.circular(15), topEnd: Radius.circular(15)),
+              image: DecorationImage(
+                image: NetworkImage(image),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * .06,
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
